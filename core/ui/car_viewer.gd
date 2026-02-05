f4b7b3bbf9cef3cb2b08802191fd7445c361af84
Rafał Kuźnia <rafal.kuznia@protonmail.com>
2026-02-05 17:36:28 +0100

    ui: add car viewer
    The car viewer scene visualizes the currently selected car in the main
    menu.

---
+++ core/ui/car_viewer.gd
@@ -0,0 +1,49 @@
+extends SubViewportContainer
+
+@export var car: PackedScene:
+	set(value):
+		car = value
+		self._on_car_changed.call_deferred()
+	get:
+		return car
+		
+@export var color_set: CarColorSet:
+	set(value):
+		color_set = value
+		self._on_car_color_changed()
+	get:
+		return color_set
+
+@onready var viewport = %CarViewport
+@onready var camera = %Camera3D
+
+var car_instance: Car
+const spin_angular_velocity = -2 * PI / 6
+
+func _ready() -> void:
+	self._on_car_changed.call_deferred()
+
+func _set_car(scene: PackedScene):
+	pass
+
+func _process(delta):
+	if self.car_instance:
+		self.car_instance.rotate(Vector3.UP, spin_angular_velocity * delta)
+		
+func _on_car_changed():
+	var rotation = Vector3()
+	if car:
+		if car_instance:
+			rotation = self.car_instance.rotation
+			car_instance.queue_free()
+			car_instance = null
+		car_instance = car.instantiate()
+		self.viewport.add_child(car_instance)
+		car_instance.position = Vector3.UP * (car_instance.dimensions().y / 2)
+		car_instance.rotation = rotation
+		car_instance.lights_on = true
+		car_instance.freeze = true
+
+func _on_car_color_changed():
+	if car_instance:
+		car_instance.color = self.color_set
