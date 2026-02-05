bb49a629e127f14c18e9b2613e8c9d904a8984fb
Rafał Kuźnia <rafal.kuznia@protonmail.com>
2026-02-05 17:41:48 +0100

    player: add color data to player data and sync state
    The player data can now be synchronized in multiplayer settings.

--- core/gameplay/player/player.gd
+++ core/gameplay/player/player.gd
@@ -5,6 +5,10 @@ extends Node
 @export var player_name: String
 @export var initial_transform: Transform3D
 @export var disable_steering := false
+@export var primary_color: Color
+@export var secondary_color: Color
+@export var interior_color: Color
+@export var driver_color: Color
 
 signal reposition_requested
 
@@ -18,10 +22,16 @@ func _ready():
 	assert(self.name.is_valid_int())
 	self.authority = self.name.to_int()
 	self.input.set_multiplayer_authority(self.name.to_int())
+	var color_set = CarColorSet.new()
+	color_set.primary = primary_color
+	color_set.secondary = secondary_color
+	color_set.interior = interior_color
+	color_set.driver = driver_color
 	var car = CarDB.get_car_by_uuid(self.car_uuid)
 	self.car = load(car.path).instantiate()
 	self.car.global_transform = initial_transform
 	self.add_child(self.car)
+	self.car.color = color_set
 	self.input.set_max_gear(self.car.max_gear())
 
 
