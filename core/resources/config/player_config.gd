e843ebc7741f916037eb0a5ed9bb2f5f252ee126
Rafał Kuźnia <rafal.kuznia@protonmail.com>
2026-02-05 17:40:34 +0100

    ui: add player color configuration to main menu
    The color is not applied to the car yet.

--- core/resources/config/player_config.gd
+++ core/resources/config/player_config.gd
@@ -3,12 +3,22 @@ extends Resource
 
 @export var player_name: String
 @export var car_uuid: String
+@export var color_primary: Color
+@export var color_secondary: Color
+@export var color_driver: Color
+@export var color_interior: Color
 
 
 func serialize() -> Dictionary:
 	return {
 		"player_name": self.player_name,
 		"car_uuid": self.car_uuid,
+		"color_set": {
+			"primary": self.color_primary,
+			"secondary": self.color_secondary,
+			"driver": self.color_driver,
+			"interior": self.color_interior,
+		}
 	}
 
 
@@ -16,4 +26,9 @@ static func deserialize(data: Dictionary) -> PlayerConfig:
 	var result = PlayerConfig.new()
 	result.player_name = data["player_name"]
 	result.car_uuid = data["car_uuid"]
+	var color_set_data = data["color_set"]
+	result.color_primary = color_set_data["primary"]
+	result.color_secondary = color_set_data["secondary"]
+	result.color_driver = color_set_data["driver"]
+	result.color_interior = color_set_data["interior"]
 	return result
--- core/ui/config/player_config.gd
+++ core/ui/config/player_config.gd
@@ -3,11 +3,15 @@ extends Control
 @export var player := PlayerConfig.new()
 
 signal player_config_changed
+signal player_car_changed
+signal player_color_changed
+signal player_name_changed
 
 const player_config_path = "user://last.player.tres"
 
 @onready var player_name = %PlayerName
 @onready var car_picker = %CarPicker
+@onready var color_picker = %CarColorPicker
 
 
 func _ready():
@@ -28,9 +32,21 @@ func save_player():
 
 func _on_car_picker_car_selected():
 	self.player.car_uuid = car_picker.selected_car
+	self.player_car_changed.emit()
 	self.player_config_changed.emit()
 
 
 func _on_player_name_text_submitted(new_text):
 	player.player_name = new_text
+	self.player_name_changed.emit()
+	self.player_config_changed.emit()
+
+
+func _on_car_color_picker_color_changed() -> void:
+	var color_set = self.color_picker.selected_color_set
+	self.player.color_primary = color_set.primary
+	self.player.color_secondary = color_set.secondary
+	self.player.color_interior = color_set.interior
+	self.player.color_driver = color_set.driver
+	self.player_color_changed.emit()
 	self.player_config_changed.emit()
