fe40f8dd64290261fc9eff2c7f8fbebd5e3fb40d
Rafał Kuźnia <rafal.kuznia@protonmail.com>
2026-02-08 17:41:48 +0100

    manifest: add ffmpeg to the bundle
    The ffmpeg is needed to convert audio samples to wav format.

--- scripts/make_bundle.sh
+++ scripts/make_bundle.sh
@@ -4,6 +4,9 @@ mkdir bundle
 
 pushd bundle
 wget https://github.com/e-rk/speedtools/releases/download/v$(jq -r '.speedtools' ../manifest.json)/speedtools-$(jq -r '.speedtools' ../manifest.json).zip
+wget https://github.com/GyanD/codexffmpeg/releases/download/$(jq -r '.ffmpeg' ../manifest.json)/ffmpeg-$(jq -r '.ffmpeg' ../manifest.json)-essentials_build.zip
+unzip ffmpeg-$(jq -r '.ffmpeg' ../manifest.json)-essentials_build.zip
+rm ffmpeg-$(jq -r '.ffmpeg' ../manifest.json)-essentials_build.zip
 popd
 
 wget https://github.com/godotengine/godot/releases/download/$(jq -r '.godot' manifest.json)-stable/Godot_v$(jq -r '.godot' manifest.json)-stable_win64.exe.zip
